b2da6d70db45882a69fa4d4022b07b619b309af8
Yasin Kinyua <yasinkinyua@gmail.com>
2022-02-01 14:11:32 +0300

    Refactor animechan get request

--- api/request.go
+++ api/request.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func GetSingleRequest(url string) QouteAnime {
+func GetRequestHandler(url string) []byte {
 	client := &http.Client{}
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -29,60 +29,27 @@ func GetSingleRequest(url string) QouteAnime {
 	if err != nil {
 		fmt.Print(err.Error())
 	}
+	return bodyBytes
+}
+
+func GetSingleRequest(url string) QouteAnime {
+	bodyBytes := GetRequestHandler(url)
+
 	var responseObject QouteAnime
 	json.Unmarshal(bodyBytes, &responseObject)
 	return responseObject
 
 }
 func GetAllRequest(url string) []QouteAnime {
-	client := &http.Client{}
+	bodyBytes := GetRequestHandler(url)
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-
-	defer resp.Body.Close()
-
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
 	var responseObject []QouteAnime
 	json.Unmarshal(bodyBytes, &responseObject)
 	return responseObject
 }
 
 func GetAllAnimeRequest(url string) []string {
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-
-	defer resp.Body.Close()
-
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
+	bodyBytes := GetRequestHandler(url)
 	var responseObject []string
 	json.Unmarshal(bodyBytes, &responseObject)
 	return responseObject
